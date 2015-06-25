94213e7f67e17b48cef4d28a6c08e953bdb8f961
skyler-gray <skylergray@flybymedia.com>
2015-06-25 14:05:06 -0400

    WIP doesn't compile

--- FutureKitTests/PromiseTests.swift
+++ FutureKitTests/PromiseTests.swift
@@ -10,6 +10,61 @@ import Foundation
 import XCTest
 import FutureKit
 
+struct TestCaseVariables<T> {
+    var promise: Promise<T>?
+    var success:T?
+    var error:ErrorType?
+    var promiseExecutor : Executor?
+    var futureExecutor : Executor?
+}
+
+enum PromiseFunctions {
+    case onRequestCancelExecutor
+    case onRequestCancel
+    case automaticallyCancelOnRequestCancel
+    case initWithAutomaticallyCancelAfter
+    case initWithAutomaticallyFailAfter
+    case initWithAutomaticallyAssertAfter
+    
+    case complete
+    case completeWithSuccess
+    case completeWithFailErrorMessage
+    case completeWithFail
+    case completeWithException
+    case completeWithCancel
+    case continueWithFuture
+    case completeWithBlock
+    
+    case failIfNotCompleted
+    case failIfNotCompletedErrorMessage
+    case isCompleted
+    
+    case tryComplete
+    case completeWithOnCompletionError
+    
+    
+    func executeWith<T>(test : TestCaseVariables<T>)
+    
+    {
+        switch self {
+
+        case completeWithSuccess:
+            test.promise!.completeWithSuccess(test.success!)
+            
+
+        default:
+            assertionFailure("haven't implemeted \(self) yet!")
+            XCTFail("haven't impleneted test case yet!")
+            
+            
+    }
+    
+    
+    
+    
+    
+}
+
 class PromiseTests: XCTestCase {
 
     override func setUp() {
@@ -23,19 +78,115 @@ class PromiseTests: XCTestCase {
     }
     
     
-    func _testPromiseSuccess<T>(success:T, promiseExecutor : Executor = .Primary, futureExecutor : Executor = .Primary) {
+    func onPromiseSuccessVoid(promiseExecutor p: Executor, futureExecutor : Executor) {
+        
+        let promise = Promise<Void>()
+        let f = promise.future
+        let success: () = ()
+        
+        let completeExpectation = self.expectationWithDescription("Future.onComplete")
+        let successExpectation = self.expectationWithDescription("Future.onSuccess")
+        let anySuccessExpectation = self.expectationWithDescription("Future.onAnySuccess")
+        
+        
+        f.onComplete(futureExecutor) { (completion) -> Void in
+            
+            switch completion {
+            case let .Success(t):
+                break
+            default:
+                XCTFail("unexpectad completion value \(completion)")
+            }
+            completeExpectation.fulfill()
+        }
+        
+        // TODO: Can we get this to compile?
+        
+/*        f.onSuccess(futureExecutor) { (result:()) -> Void in
+            
+            successExpectation.fulfill()
+            
+        } */
+        f.onAnySuccess(futureExecutor) { (result) -> Void in
+            XCTAssert(result is Void, "Didn't get expected success value \(success)")
+            anySuccessExpectation.fulfill()
+        }
+        
+        f.onFail(futureExecutor) { (error) -> Void in
+            XCTFail("unexpectad onFail error \(error)")
+            
+        }
+        f.onCancel(futureExecutor) { () -> Void in
+            XCTFail("unexpectad onCancel" )
+        }
+        
+        promise.completeWithSuccess(success)
+        
+        self.waitForExpectationsWithTimeout(0.5, handler: nil)
+        
+    }
+
+    func onPromiseSuccess<T : Equatable>(success:T, promiseExecutor : Executor, futureExecutor : Executor) {
         
         let promise = Promise<T>()
-        let future = promise.future
+        let f = promise.future
         
         
-        future
+        let completeExpectation = self.expectationWithDescription("Future.onComplete")
+        let successExpectation = self.expectationWithDescription("Future.onSuccess")
+        let anySuccessExpectation = self.expectationWithDescription("Future.onAnySuccess")
+        
+        
+        f.onComplete(futureExecutor) { (completion) -> Void in
+            
+            switch completion {
+            case let .Success(t):
+                XCTAssert(t.result == success, "Didn't get expected success value \(success)")
+                XCTAssert(completion.result == success, "Didn't get expected success value \(success)")
+            default:
+                XCTFail("unexpectad completion value \(completion)")
+            }
+            completeExpectation.fulfill()
+        }
+        
+        f.onSuccess(futureExecutor) { (result) -> Void in
+            
+            XCTAssert(result == success, "Didn't get expected success value \(success)")
+            successExpectation.fulfill()
+            
+        }
+        f.onAnySuccess(futureExecutor) { (result) -> Void in
+            let r = result as! T
+            XCTAssert(r == success, "Didn't get expected success value \(success)")
+            anySuccessExpectation.fulfill()
+            
+        }
+        
+        f.onFail(futureExecutor) { (error) -> Void in
+            XCTFail("unexpectad onFail error \(error)")
+            
+        }
+        f.onCancel(futureExecutor) { () -> Void in
+            XCTFail("unexpectad onCancel" )
+        }
         
         promiseExecutor.execute {
             promise.completeWithSuccess(success)
         }
         
+        self.waitForExpectationsWithTimeout(0.5, handler: nil)
         
+    }
+    
+    func testPromiseSuccess()  {
+
+
+        self.onPromiseSuccess(0, promiseExecutor: .Primary, futureExecutor: .Primary)
+        self.onPromiseSuccess("String", promiseExecutor: .Primary, futureExecutor: .Primary)
+        self.onPromiseSuccess([1,2], promiseExecutor: .Primary, futureExecutor: .Primary)
+
+        self.onPromiseSuccessVoid(promiseExecutor: .Primary, futureExecutor: .Primary)
+
     }
 
     func testExample() {

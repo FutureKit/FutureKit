0e7a78ec229f30579210d8d1a217acb4c27467c9
andystanton <andystanton@gmail.com>
2017-02-03 21:12:28 +0000

    Exclude invalid paths in Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,19 @@ import PackageDescription
 
 let package = Package(
     name: "FutureKit",
-    dependencies: []
+    dependencies: [],
+    exclude: [
+      "docs",
+      "FutureKit/FutureKit.h",
+      "FutureKit/Utils/ObjectiveCExceptionHandler.h",
+      "FutureKit/Utils/ObjectiveCExceptionHandler.m",
+      "FutureKit iOS Testing AppTests",
+      "FutureKit iOS Tests",
+      "FutureKit OSX Tests",
+      "FutureKit tvOS",
+      "FutureKit tvOSTests",
+      "FutureKit watchOS",
+      "FutureKit.xcworkspace",
+      "FutureKitTests"
+    ]
 )

d160222da9b2fa47099ec3ae5c8b313abb5c66f0
Kevin Lundberg <klundberg@wayfair.com>
2017-11-14 22:42:26 -0500

    Don't run tests on linux for now since they require ObjC

--- Package.swift
+++ Package.swift
@@ -1,11 +1,18 @@
 import PackageDescription
 
-let package = Package(
-    name: "FutureKit",
-    targets: [
-        Target(name: "FutureKit", dependencies: []),
+var targets = [
+    Target(name: "FutureKit", dependencies: []),
+]
+
+#if !os(Linux)
+    targets += [
         Target(name: "BlockBasedTestCase", dependencies: []),
         Target(name: "FutureKitTests", dependencies: ["FutureKit", "BlockBasedTestCase"]),
-    ],
+    ]
+#endif
+
+let package = Package(
+    name: "FutureKit",
+    targets: targets,
     dependencies: []
 )

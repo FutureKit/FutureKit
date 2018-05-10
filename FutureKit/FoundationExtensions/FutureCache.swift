dff9bbe0f64142e2424bcdc798e237ef8de5a2eb
Michael Gray <michael@futurekit.org>
2018-05-10 12:37:34 -0400

    some features to FutureCache

--- FutureKit/FoundationExtensions/FutureCache.swift
+++ FutureKit/FoundationExtensions/FutureCache.swift
@@ -27,48 +27,75 @@ open class FutureCacheEntry<T> {
     }
 
     public init(value: T,expireTime e: Date? = nil) {
-        self.future = Future(success: value)
+        self.future = .success(value)
         self.expireTime = e
     }
 
 }
 
 
-open class FutureCache<KeyType : AnyObject, T> {
+private class ObjectWrapper {
+    let value: Any
+
+    init(_ value: Any) {
+        self.value = value
+    }
+}
+
+private class KeyWrapper<KeyType: Hashable>: NSObject {
+    let key: KeyType
+    init(_ key: KeyType) {
+        self.key = key
+    }
+
+    override var hash: Int {
+        return key.hashValue
+    }
+
+    override func isEqual(_ object: Any?) -> Bool {
+        guard let other = object as? KeyWrapper<KeyType> else {
+            return false
+        }
+        return key == other.key
+    }
+}
+
+
+public class FutureCache<KeyType : Hashable, T> {
     
     
     public init () {
         
     }
-    var cache = NSCache<KeyType,FutureCacheEntry<T>>()
-    
-    
-    open func object(forKey key: KeyType) -> T? {
-        return cache.object(forKey: key)?.future.value
+
+    private let cache: NSCache<KeyWrapper<KeyType>, FutureCacheEntry<T>> = NSCache()
+
+    public func object(forKey key: KeyType) -> T? {
+        return cache.object(forKey: KeyWrapper(key))?.future.value
     }
 
-    open func setObject(_ obj: T, forKey key: KeyType) {
-        let entry = FutureCacheEntry(value:obj)
-        cache.setObject(entry, forKey: key)
+    public func setObject(_ obj: T, forKey key: KeyType) {
+        let entry = FutureCacheEntry(value: obj)
+        cache.setObject(entry, forKey: KeyWrapper(key))
     }
 
-    open func setObject(_ obj: T, forKey key: KeyType, cost g: Int) {
+    public func setObject(_ obj: T, forKey key: KeyType, cost g: Int) {
         let entry = FutureCacheEntry(value:obj)
-        cache.setObject(entry, forKey: key, cost: g)
+        cache.setObject(entry, forKey: KeyWrapper(key), cost: g)
     }
     
-    open func removeObject(forKey key: KeyType) {
-        cache.removeObject(forKey: key)
+    public func removeObject(forKey key: KeyType) {
+        cache.removeObject(forKey: KeyWrapper(key))
     }
     
-    open func removeAllObjects() {
+    public func removeAllObjects() {
         cache.removeAllObjects()
     }
     
 
-    fileprivate func _findOrFetch(key : KeyType, expireTime: Date? = nil, onFetch:() -> Future<T>) -> FutureCacheEntry<T> {
-        
-        if let entry = self.cache.object(forKey: key) {
+    private func getCacheEntry(wrappedKey : KeyWrapper<KeyType>, onFetch:() -> Future<T>, mapExpireTime: ((FutureResult<T>) -> Date?)? = nil) -> FutureCacheEntry<T> {
+
+        if let entry = self.cache.object(forKey: wrappedKey) {
             if let expireTime = entry.expireTime {
                 if expireTime.timeIntervalSinceNow > 0 {
                     return entry
@@ -79,15 +106,31 @@ open class FutureCache<KeyType : AnyObject, T> {
             }
         }
         let f = onFetch()
-        let entry = FutureCacheEntry(f,expireTime: expireTime)
+        let entry = FutureCacheEntry(f)
         // it's important to call setObject before adding the onFailorCancel handler, since some futures will fail immediatey!
-        self.cache.setObject(entry, forKey: key)
-        f.onFailorCancel { (result) -> Void in
-            self.cache.removeObject(forKey: key)
+        self.cache.setObject(entry, forKey: wrappedKey)
+
+        f.onComplete { result in
+            if T.self is HasCacheCost.Type {
+                if let cacheCost = (result.value as? HasCacheCost)?.cacheCost {
+                    self.cache.setObject(entry, forKey: wrappedKey, cost: cacheCost)
+                }
+            }
+            if let expireTime = mapExpireTime?(result) {
+                entry.expireTime = expireTime
+            } else if !result.isSuccess {
+                self.cache.removeObject(forKey: wrappedKey)
+            }
         }
         return entry
     }
-    
+
+    public func findOrFetch(key : KeyType, mapExpireTime: @escaping ((FutureResult<T>) -> Date?), onFetch:() -> Future<T>) -> Future<T> {
+
+        let wrappedKey = KeyWrapper(key)
+        return getCacheEntry(wrappedKey: wrappedKey, onFetch: onFetch, mapExpireTime: mapExpireTime).future
+    }
+
     /**
     Utlity method for storing "Futures" inside a NSCache
      
@@ -99,31 +142,41 @@ open class FutureCache<KeyType : AnyObject, T> {
      - returns: Either a copy of the cached future, or the result of the onFetch() block
      */
     public func findOrFetch(key : KeyType, expireTime: Date? = nil, onFetch:() -> Future<T>) -> Future<T> {
-        
-        return _findOrFetch(key: key, expireTime: expireTime,onFetch: onFetch).future
-    }
-}
 
-public  extension FutureCache where T:HasCacheCost {
-
-    /**
-     Utlity method for storing "Futures" inside a NSCache
-     
-     - parameter key:        key
-     - parameter expireTime: an optional date that this key will 'expire'
-     There is no logic to 'flush' expired keys.  They are just checked when retreived.
-     - parameter onFetch:    A block to execute when the cache doesn't contain the key.
-     
-     - returns: Either a copy of the cached future, or the result of the onFetch() block
-     */
-    public func findOrFetch(key : KeyType, expireTime: Date? = nil, onFetch:() -> Future<T>) -> Future<T>  {
-        
-        let entry =  _findOrFetch(key: key,expireTime: expireTime,onFetch: onFetch)
-        return entry.future.onSuccess { value in
-            self.cache.setObject(entry, forKey: key, cost: value.cacheCost)
-            return value
+        let mapExpireTime: ((FutureResult<T>) -> Date?)
+        if let expireTime = expireTime {
+            mapExpireTime = { (result) -> Date? in
+                switch result {
+                case .success:
+                    return expireTime
+                default:
+                    return nil
+                }
+            }
+        } else {
+            mapExpireTime = { _ in return nil }
         }
+
+        return self.findOrFetch(key: key, mapExpireTime:mapExpireTime, onFetch: onFetch)
     }
 
-    
+    public func findOrFetch(key : KeyType, expireAfter: TimeInterval, onFailExpireAfter: TimeInterval? = nil, onFetch:() -> Future<T>) -> Future<T> {
+
+        return self.findOrFetch(key: key,
+                                mapExpireTime: { (result) -> Date? in
+                                    switch result {
+                                    case .success:
+                                        return Date(timeIntervalSinceNow: expireAfter)
+                                    case .fail:
+                                        return onFailExpireAfter.flatMap { Date(timeIntervalSinceNow: $0) }
+                                    case .cancelled:
+                                        return nil
+                                    }
+        }, onFetch: onFetch)
+
+    }
+
+
+
 }
+
